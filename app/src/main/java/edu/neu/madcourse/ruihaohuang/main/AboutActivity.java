193f1c7101e5a691319d347bc827a500c283e970
Huang Ruihao <huangruihao@HuangdeMacBook-Pro.local>
2017-01-20 19:51:40 -0500

    Added src files to git again.

---
+++ app/src/main/java/edu/neu/madcourse/ruihaohuang/main/AboutActivity.java
@@ -0,0 +1,117 @@
+package edu.neu.madcourse.ruihaohuang.main;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.telephony.TelephonyManager;
+import android.text.Html;
+import android.text.Spanned;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import edu.neu.madcourse.ruihaohuang.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    private final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getResources().getStringArray(R.array.my_information)[0]);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+
+        ((TextView) findViewById(R.id.text_basic_information))
+                .setText(fromHtml(getMyBasicInformation()));
+        ((ImageView) findViewById(R.id.image_head_shot))
+                .setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
+                        R.drawable.about_head_shot));
+
+        // Initialize its content to PERMISSION DENIED
+        ((TextView) findViewById(R.id.text_IMEI))
+                .setText(String.format(getString(R.string.description_IMEI),
+                        getString(R.string.info_permission_denied)));
+        // Check for permission
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.READ_PHONE_STATE);
+        // Failed: request for new permission
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(AboutActivity.this,
+                    Manifest.permission.READ_PHONE_STATE)) {
+                Toast.makeText(getApplicationContext(),
+                        getString(R.string.toast_request_permission_read_phone_state),
+                        Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(AboutActivity.this,
+                        new String[] {Manifest.permission.READ_PHONE_STATE},
+                        MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
+            }
+        } else {  // Succeed: access IMEI with permission
+            ((TextView) findViewById(R.id.text_IMEI))
+                    .setText(String.format(getString(R.string.description_IMEI),
+                            ((TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId()));
+        }
+    }
+
+    String getMyBasicInformation() {
+        String html = "";
+        String[] descriptions = getResources().getStringArray(R.array.descriptions);
+        String[] myInformation = getResources().getStringArray(R.array.my_information);
+        for (int i = 0; i < descriptions.length; ++i) {
+            html += "<p>";
+            html += String.format(descriptions[i], myInformation[i]);
+            html += "</p>";
+        }
+
+        return html;
+    }
+
+    /**
+     * Reference: http://stackoverflow.com/questions/37904739/html-fromhtml-deprecated-in-android-n
+     * @param html a html string
+     * @return Spanned object that can be formatted by TextView
+     */
+    @SuppressWarnings("deprecation")
+    public static Spanned fromHtml(String html){
+        Spanned result;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            result = Html.fromHtml(html,Html.FROM_HTML_MODE_LEGACY);
+        } else {
+            result = Html.fromHtml(html);
+        }
+        return result;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_PHONE_STATE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    ((TextView) findViewById(R.id.text_IMEI))
+                            .setText(String.format(getString(R.string.description_IMEI),
+                                    ((TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId()));
+                }
+                return;
+            default:
+        }
+    }
+}
---
+++ app/src/main/java/edu/neu/madcourse/ruihaohuang/tictactoe/TicTacToeMainActivity.java
@@ -0,0 +1,54 @@
+/***
+ * Excerpted from "Hello, Android",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/eband4 for more book information.
+***/
+package edu.neu.madcourse.ruihaohuang.tictactoe;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import edu.neu.madcourse.ruihaohuang.R;
+
+// this name is to distinguish with the real MainActivity
+public class TicTacToeMainActivity extends Activity {
+   MediaPlayer mMediaPlayer;
+   // ...
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+
+       // Fullscreen
+       // Reference: http://stackoverflow.com/questions/2591036/how-to-hide-the-title-bar-for-an-activity-in-xml-with-existing-custom-theme
+       this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+       this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+               WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+       setContentView(R.layout.activity_tictactoe_main);
+
+   }
+
+   @Override
+   protected void onResume() {
+      super.onResume();
+      mMediaPlayer = MediaPlayer.create(this, R.raw.tictactoe_cute);
+      mMediaPlayer.setVolume(0.5f, 0.5f);
+      mMediaPlayer.setLooping(true);
+      mMediaPlayer.start();
+   }
+
+   @Override
+   protected void onPause() {
+      super.onPause();
+      mMediaPlayer.stop();
+      mMediaPlayer.reset();
+      mMediaPlayer.release();
+   }
+}
