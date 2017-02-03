1d39f4c80ef9e49437b41255f1b7e0b23b3899dc
Huang Ruihao <huangruihao@HuangdeMacBook-Pro.local>
2017-02-02 19:57:53 -0500

    Resolved #17.

--- app/src/main/java/edu/neu/madcourse/ruihaohuang/dictionary/DictionaryActivity.java
+++ app/src/main/java/edu/neu/madcourse/ruihaohuang/dictionary/DictionaryActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import edu.neu.madcourse.ruihaohuang.R;
@@ -17,6 +18,8 @@ public class DictionaryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dictionary);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DictionaryHelper helper = DictionaryHelper.getInstance();
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
