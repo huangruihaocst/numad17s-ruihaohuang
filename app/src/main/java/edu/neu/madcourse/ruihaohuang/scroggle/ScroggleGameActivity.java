a3936a5adb84a8f60ecdc719402fd849b7683e86
Huang Ruihao <huangruihao@HuangdeMacBook-Pro.local>
2017-02-15 19:17:35 -0500

    Resolved #39.

--- app/src/main/java/edu/neu/madcourse/ruihaohuang/scroggle/ScroggleGameActivity.java
+++ app/src/main/java/edu/neu/madcourse/ruihaohuang/scroggle/ScroggleGameActivity.java
@@ -1,17 +1,20 @@
 package edu.neu.madcourse.ruihaohuang.scroggle;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import edu.neu.madcourse.ruihaohuang.R;
 
-public class ScroggleMainActivity extends AppCompatActivity {
+public class ScroggleGameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scroggle_main);
+        setContentView(R.layout.activity_scroggle_game);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -23,7 +26,6 @@ public class ScroggleMainActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
-
     }
 
 }
---
+++ app/src/main/java/edu/neu/madcourse/ruihaohuang/scroggle/Tile.java
@@ -0,0 +1,9 @@
+package edu.neu.madcourse.ruihaohuang.scroggle;
+
+/**
+ * Created by huangruihao on 2017/2/15.
+ */
+
+public class Tile {
+
+}
